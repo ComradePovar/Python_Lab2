99c3ef21a8a23bfc7e452603750d746fd893fc3e
Tokarev Maxim <nepfion96@gmail.com>
2017-10-03 22:11:37 +0300

    upd

--- Main.py
+++ Main.py
@@ -1,12 +1,6 @@
 import os
-import time
 from datetime import datetime
 import os.path as path
-
-
-def addline(buf, line):
-    buf.pop(0)
-    buf.append(line)
 
 datetimePattern = "%d.%m.%Y %H:%M:%S"
 directory = ""
@@ -14,6 +8,7 @@ requiredExtension = input("Enter extension: ")
 requiredDate = ""
 requiredString = ""
 requiredTime = 0
+stringCount = 2
 
 while True:
     directory = input("Enter directory: ")
@@ -44,27 +39,43 @@ else:
 
 for fileName in files:
     with open(fileName) as file:
-        line = file.readline()
-        buffer = ["", "", "", "", ""]
+        search_result = []
+        line_offsets = []
+        line = '.'
         requiredStringIndex = 1
         isFound = False
         while line != '' and not isFound:
-            addline(buffer, line)
+            line_offsets.append(file.tell())
+            line = file.readline()
             if line == requiredString:
                 isFound = True
-                for i in range(0, 2):
+                requiredStringIndex = len(line_offsets) - 1
+                requiredStringCount = 0
+
+                while line != '' and line == requiredString:
+                    requiredStringCount += 1
+                    line_offsets.append(file.tell())
                     line = file.readline()
-                    if line != ' ':
-                        addline(buffer, line)
-                    else:
+
+                lowerBound = max(0, requiredStringIndex - requiredStringCount * stringCount)
+                print (lowerBound)
+                file.seek(line_offsets[lowerBound])
+                for i in range(lowerBound, requiredStringIndex + requiredStringCount):
+                    search_result.append(file.readline())
+                i = 0
+                while i < requiredStringCount * stringCount:
+                    line = file.readline()
+                    if line == '':
                         break
-                break
-            line = file.readline()
-            requiredStringIndex += 1
+                    search_result.append(line)
+                    i += 1
+
         if isFound:
             print ("Fullname: {}".format(path.join(directory, fileName)))
             print ("Creation date: {}".format(datetime.fromtimestamp(path.getctime(fileName)).strftime(datetimePattern)))
             print ("String number: {}".format(requiredStringIndex))
             print ("Content:")
-            for i in range(0, 5):
-                print(buffer[i])
+            for i in range(0, len(search_result)):
+                print(search_result[i])
+        else:
+            print ("Required string not found.")
